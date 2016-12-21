78ab5e42506118c854a9e35a66fa8ad12da9e547
jackliddiard <jack.liddiard@iiincubator.com>
2016-12-21 08:26:21 +0000

    Fixed imporrs

--- api/dao/memory/post.go
+++ api/dao/memory/post.go
@@ -1,24 +1,23 @@
 package memory
 
 import (
-	"github.com/deadcore/go-blog/model"
-	"github.com/kataras/go-errors"
+	"errors"
+	"github.com/deadcore/go-blog/api/model"
 )
 
 type (
 	InMemoryPostDao struct {
-
 	}
 )
 
 var (
 	arr = make([]model.Post, 16)
-	c count64
+	c   count64
 )
 
 func (m *InMemoryPostDao) Get(id uint64) (model.Post, error) {
 	return findOne(func(post model.Post) bool {
-			return post.Id == id
+		return post.Id == id
 	})
 }
 
@@ -31,7 +30,7 @@ func (m *InMemoryPostDao) Save(post model.Post) model.Post {
 func findOne(f func(post model.Post) bool) (model.Post, error) {
 	for _, v := range arr {
 		if f(v) {
-			return v, nil;
+			return v, nil
 		}
 	}
 	return model.Post{}, errors.New("NOT FOUND")
